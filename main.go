7abeee1a709be26969c4986070fc28d2bd8734fd
Zelalem Mekonnen <zelliott33@gmail.com>
2023-04-14 14:13:46 -0500

    change the table headers

--- main.go
+++ main.go
@@ -9,11 +9,10 @@ import (
 
 var (
 	data = [][]interface{}{
-		{1, "Newton G. Goetz", 543.4},
-		{2, "Rebecca R. Edney", 1423.25},
-		{3, "John R. Jackson", 7526.12},
-		{4, "Ron J. Gomes", 123.84},
-		{5, "Penny R. Lewis", 3221.11},
+		{1, "Talk with Asfaw", true},
+		{2, "Email R. Edney", true},
+		{3, "Go to Jackson's house", false},
+		{4, "Ron games", false},
 	}
 )
 
@@ -24,29 +23,19 @@ func main() {
 	table.Header = &simpletable.Header{
 		Cells: []*simpletable.Cell{
 			{Align: simpletable.AlignCenter, Text: "#"},
-			{Align: simpletable.AlignCenter, Text: "NAME"},
-			{Align: simpletable.AlignCenter, Text: "TAX"},
+			{Align: simpletable.AlignCenter, Text: "Task Name"},
+			{Align: simpletable.AlignCenter, Text: "Completed"},
 		},
 	}
 
-	subtotal := float64(0)
 	for _, row := range data {
 		r := []*simpletable.Cell{
 			{Align: simpletable.AlignRight, Text: fmt.Sprintf("%d", row[0].(int))},
 			{Text: row[1].(string)},
-			{Align: simpletable.AlignLeft, Text: fmt.Sprintf("$ %.2f", row[2].(float64))},
+			{Align: simpletable.AlignLeft, Text: fmt.Sprintf("%t", row[2])},
 		}
 		table.Body.Cells = append(table.Body.Cells, r)
-		subtotal += row[2].(float64)
 
-	}
-
-	table.Footer = &simpletable.Footer{
-		Cells: []*simpletable.Cell{
-			{},
-			{Align: simpletable.AlignRight, Text: "Subtotal"},
-			{Align: simpletable.AlignRight, Text: fmt.Sprintf("$ %.3f", subtotal)},
-		},
 	}
 
 	table.SetStyle(simpletable.StyleCompactLite)
